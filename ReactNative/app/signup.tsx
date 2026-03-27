b380c6a399108bd9df70994173caccc94c9e7a6b
juan6park <juan6park@gmail.com>
2026-03-27 11:12:44 +0900

    0327 - 11:12

--- ReactNative/app/signup.tsx
+++ ReactNative/app/signup.tsx
@@ -1,11 +1,29 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, StyleSheet, TextInput, TouchableOpacity } from 'react-native'
+import React, {useState} from 'react'
+import { MaterialCommunityIcons } from '@expo/vector-icons'
+import { useRouter } from 'expo-router'
+import Checkbox from 'expo-checkbox';
+
+const styles = StyleSheet.create({
+  screen:{
+    
+  },
+});
 
 const signup = () => {
+  const [isPasswordVisible, setIsPasswordVisible] = useState(false);
+  const [isChecked, setChecked] = useState(false);
+  const router = useRouter();
+
   return (
-    <View>
-      <Text>signup</Text>
-    </View>
+      <View style={styles.screen}>
+
+      </View>
+
+
+
+
+
   )
 }
 
